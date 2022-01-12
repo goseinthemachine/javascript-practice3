7b197839ac2f09479a341db19c5ffa579d136bed
Cameron Gose <cgose@bitwiseindustries.com>
2022-01-11 20:39:14 -0800

    add files

--- js/app2.js
+++ js/app2.js
@@ -1,1 +1,6 @@
-alert("This is an alert from APP 2");
+// This is a comment and will be ignored by the browser
+
+// TODO: Delete this after implementing feature 22 
+let userPhoneNumber = "555-555-5555";
+
+// alert(userPhoneNumber);
