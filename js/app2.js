d42f35098785c630e0fa63e0444ca4d822e63809
Cameron Gose <cgose@bitwiseindustries.com>
2022-01-13 18:56:34 -0800

    add console.log

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,5 @@
 alert("This is an alert from APP 2");
+console.log("Added from cli merge and push to main")
 // This is a comment and will be ignored by the browser
 
 // TODO: Delete this after implementing feature 22 
